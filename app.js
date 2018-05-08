8c83c228d6b17b488f09ea20b6cb07d62871108a
Clément Dubrisay <clement.dubrisay@gmail.com>
2018-05-08 08:49:27 +0200

    Backend - Basic Set Up

--- app.js
+++ app.js
@@ -4,21 +4,19 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const cors         = require('cors');
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
+const passportSetup  = require('./passport/setup');
     
-
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/cita-backend', {useMongoClient: true})
+  .connect(process.env.database_url, {useMongoClient: true})
   .then(() => {
-    console.log('Connected to Mongo!')
+    console.log('CHAMPIONS IN THE ARENA IS RUNING :)')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
@@ -34,35 +32,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
-});
-  
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
+// might be important to be before session (?)
+app.use(cors());
 
 // Enable authentication using session + passport
 app.use(session({
@@ -71,12 +51,10 @@ app.use(session({
   saveUninitialized: true,
   store: new MongoStore( { mongooseConnection: mongoose.connection })
 }))
-app.use(flash());
-require('./passport')(app);
-    
 
-const index = require('./routes/index');
-app.use('/', index);
+// require('./passport')(app);
+passportSetup(app);
+    
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
--- models/User.js
+++ models/User.js
@@ -5,11 +5,9 @@ const userSchema = new Schema({
   username: String,
   password: String
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
+  timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
