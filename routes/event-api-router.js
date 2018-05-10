9d757dd067bad85d3efd607de5dd1bb347631067
Clément Dubrisay <clement.dubrisay@gmail.com>
2018-05-10 16:51:33 +0200

    Frontend event creation process done

--- routes/event-api-router.js
+++ routes/event-api-router.js
@@ -22,4 +22,24 @@ router.get('/events', (req, res, next) => {
     });
 });
 
+// POST /api/events
+router.post('/events', (req, res, next) => {
+    console.log("-------------------------------------------------------------------------");
+    console.log("-------------------------------------------------------------------------");
+    console.log(req.body);
+    console.log("-------------------------------------------------------------------------");
+    console.log("-------------------------------------------------------------------------");
+
+    const { title, admin, tags, nbSelectas, selectas, nbRounds, rounds, status } = req.body;
+
+    Event.create({ title, admin, tags, nbSelectas, selectas, nbRounds, rounds, status })
+    .then((addedEvent) => {
+        res.json(addedEvent);
+    })
+    .catch((err) => {
+        next(err);
+    });
+
+});
+
 module.exports = router;
